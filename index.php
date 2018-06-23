64a6a2b2d0079816d35e7002a983eb87003a4f90
Sasha Drobot <alex.drobot22@gmail.com>
2018-06-24 01:21:56 +0300

    v 0.3 styles

--- index.php
+++ index.php
@@ -1,34 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-    <?php
-        require('fm.php');
+    <meta charset="UTF-8">
+        <?php
+            require('fm.php');
 
-        isset($_GET['to']) ? $to = $_GET['to'] : $to = getcwd();
-        isset($_GET['sort']) ? $sort = $_GET['sort'] : $sort = 'name';
-        isset($_GET['order']) ? $order = $_GET['order'] : $order = 'asc';
+            isset($_GET['to']) ? $to = $_GET['to'] : $to = getcwd();
+            isset($_GET['sort']) ? $sort = $_GET['sort'] : $sort = 'name';
+            isset($_GET['order']) ? $order = $_GET['order'] : $order = 'asc';
 
-        $filemanager = Files::getInstance($to);
-        $files = $filemanager->files;
+            $filemanager = Files::getInstance($to);
+            $files = $filemanager->files;
 
-        if (isset($_GET['sort']) && isset($_GET['order'])) {
-            $files = $filemanager->sort($files, $sort, $order);
+            if (isset($_GET['sort']) && isset($_GET['order'])) {
+                $files = $filemanager->sort($files, $sort, $order);
+            }
+
+            isset($_GET['to']) ? $path = $_GET['to'] : $path = dirname(__FILE__);
+        ?>
+    <title>My File Manager for Opinion Corp</title>
+    <style>
+        select {
+            height: 25px;
         }
-
-        isset($_GET['to']) ? $path = $_GET['to'] : $path = dirname(__FILE__);
-    ?>
-  <title>My File Manager</title>
+        .btn {
+            background: #424242;
+            border: none;
+            display: inline-block;
+            cursor: pointer;
+            padding: 5px 15px 5px 15px;
+            border-radius: 5px;
+            color: white;
+            transition: all .5s linear;
+        }
+        .btn:hover {
+            background: #212121;
+        }
+        table {
+            margin-top: 20px;
+            border-collapse: collapse;
+            border-left: 3px solid #424242;
+            border-right: 3px solid #424242;
+            border-bottom: 3px solid #424242;
+            font-family: "Lucida Grande", sans-serif;
+        }
+        table thead {
+            background: #424242;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            padding: 10px;
+            box-shadow: 0 2px 4px 0 rgba(0,0,0,.3);
+            color: white;
+            font-family: "Roboto Slab",serif;
+            font-style: normal;
+            font-size: 20px;
+            text-align: center;
+            margin: 0;
+        }
+        table td, table th {
+            padding: 10px;
+        }
+        table th {
+            text-align: center;
+            font-size: 18px;
+        }
+        table tbody tr {
+            cursor: pointer;
+            transition: all .5s linear;
+        }
+        table tbody tr:hover {
+            background: #E5E5E5;
+        }
+        table tr:nth-child(2n) {
+            background: #f5f5f5;
+        }
+        table td:last-of-type {
+            text-align: center;
+        }
+        table tr td a {
+            text-decoration: none;
+            color: #212121;
+            font-weight: bold;
+        }
+        p a {
+            text-decoration: none;
+            color: #212121;
+            transition: all .5s linear;
+            display: inline-block;
+            border: #424242 2px solid;
+            padding: 5px 15px 5px 15px;
+            font-weight: bold;
+        }
+        p a:hover {
+            color: #E5E5E5;
+            background: #424242;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body>
-    <p>
-        <?php echo $path."<br/>";
-          if ($path !== dirname(__FILE__))
-          {
-            $back = $_SERVER['PHP_SELF']."?to=".realpath($_GET['to'].'/../');
-            echo "<a href=".$back.">Back</a>";
-          } ?>
-    </p>
     <form method="get" action="">
         <select name="sort" required>
             <option value="name">Name</option>
@@ -40,7 +110,7 @@
             <option value="desc">descending</option>
         </select>
         <input type="hidden" name="to" value="<?php echo $to ?>">
-        <input type="submit" value="Sort">
+        <input type="submit" value="Sort" class="btn">
     </form>
 
     <table>
@@ -50,9 +120,9 @@
         else { ?>
             <thead>
                 <tr>
-                    <td><strong>Name</strong></td>
-                    <td><strong>Type</strong></td>
-                    <td><strong>Size</strong></td>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Size</th>
                 </tr>
             </thead>
           <tbody>
@@ -86,5 +156,13 @@
           <?php endforeach; }?>
            </tbody>
     </table>
+    <p>
+        <?php
+        if ($path !== dirname(__FILE__))
+        {
+            $back = $_SERVER['PHP_SELF']."?to=".realpath($_GET['to'].'/../');
+            echo "<a href=".$back.">&larr; back</a>";
+        } ?>
+    </p>
 </body>
 </html>
